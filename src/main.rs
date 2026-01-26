32526c63fc5db912807a9d1de4211381cbfce714
22deeme22 <dario.marcone@epfl.ch>
2026-01-26 17:27:39 +0100

    Started cryptography
    I starded to encrypt datas with ChaCha20-poly1305, but for now there is errors yet because I don't ask a key to the user, I have to do this, encrypt the key of the user, I didn't chose yet with which algorithms. Then, when i'll do this, the first version will almost be done.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use serde::{Serialize, Deserialize};
-use std::fs;
-use serde_json::{from_str, from_value, to_string_pretty};
+use std::{fs, u8};
+use serde_json::{from_str, to_string_pretty};
 use clap::{Parser};
+use chacha20poly1305::{
+    ChaCha20Poly1305, Nonce, aead::{Aead, AeadCore, KeyInit, OsRng}
+};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Entry {
@@ -20,49 +23,92 @@ struct Cli {
 
 #[derive(Parser)]
 enum Commands {
+    /// Add an entry of service (-s), login (-l) and password (-p) specified.
     Add {
-        /// Enter service here.
-        #[arg(long, short)]
+        #[arg(short)]
         service: String,
-        /// Enter your login.
-        #[arg(long, short)]
+        #[arg(short)]
         login: String,
-        /// Enter your password.
-        #[arg(long, short)]
+        #[arg(short)]
         password: String,
     },
+    /// Delete the entry of the service specified.
     Remove{
-        /// Enter service here.
-        #[arg(long, short)]
         service: String
     },
+    /// List every entry.
     List,
 }
 
 fn main() {
+    let key = ChaCha20Poly1305::generate_key(&mut OsRng);
+    let cipher = ChaCha20Poly1305::new(&key);
+ 
+    
     let cli = Cli::parse();
     match cli.command {
         Commands::Add { service, login, password } => {
-            let value = fs::read_to_string("data/data.json").expect("error");
-            let mut entries = from_str::<Vec<Entry>>(&value).expect("error");
-            entries.push(Entry{ service, login, password });
-            let json = to_string_pretty(&entries).expect("error");
-            fs::write("data/data.json", json).expect("error");
+
+                                                                      
+            // Take the vector of entry that is already in the file
+            let data = fs::read("data/data.json").expect("1");
+            let mut entries = Vec::new();
+            if !data.is_empty() {
+                entries = decrypt(&data, &cipher);
+            } 
+            
+            // Add the entry that the user wrote to the vector
+            entries.push(Entry{ service, login, password});
+
+            let encrypted = encrypt(&entries, &cipher);
+           
+            fs::write("data/data.json", encrypted).expect("6");
         }
 
         Commands::Remove { service } => {
-            let value = fs::read_to_string("data/data.json").expect("error");
-            let mut entries = from_str::<Vec<Entry>>(&value).expect("error");
+            let data = fs::read("data/data.json").expect("1");
+            
+            let mut entries = decrypt(&data, &cipher);
+        
+            // Keep every entry where the service doesn't correspond to the one that the user want to remove
             entries.retain(|entry| entry.service != service);
-            let json = to_string_pretty(&entries).expect("error");
-            fs::write("data/data.json", json).expect("error");
+
+            let encrypted = encrypt(&entries, &cipher);
+
+            fs::write("data/data.json", encrypted).expect("6");
         }
 
         Commands::List => {
-            let value = fs::read_to_string("data/data.json").expect("error");
-            let entries = from_str::<Vec<Entry>>(&value).expect("error");
+            // Take the vector of entry that is already in the file
+            let data = fs::read("data/data.json").expect("1");
+            
+            let entries = decrypt(&data, &cipher);
+            // Print the vector
             println!("{:#?}", entries);
         }
-        
     }
+}
+
+fn encrypt(entries: &Vec<Entry>, cipher: &ChaCha20Poly1305) -> Vec<u8> {
+    let e_nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
+
+    // Rewrote the vector with the value added to the data file
+    let json = to_string_pretty(&entries).expect("4");
+            
+    let encrypted = cipher.encrypt(&e_nonce, json.as_bytes()).expect("55555");
+
+    let mut out = Vec::new();
+    out.extend_from_slice(&e_nonce);
+    out.extend_from_slice(&encrypted);
+    out
+}
+
+
+fn decrypt(data: &Vec<u8>, cipher: &ChaCha20Poly1305) -> Vec<Entry> {
+    let (d_nonce_bytes, ciphertext) = data.split_at(12);
+    let d_nonce = Nonce::from_slice(d_nonce_bytes);
+                
+    let decrypted = cipher.decrypt(&d_nonce, ciphertext).expect("22"); 
+    let json = String::from_utf8(decrypted).expect("2"); 
+    from_str::<Vec<Entry>>(&json).expect("3")
 }
