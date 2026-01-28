510f56f8cfe69e51caa55e79a6c000cd6adf0a82
22deeme22 <dario.marcone@epfl.ch>
2026-01-28 11:17:16 +0100

    Added comments and epure the code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use serde::{Serialize, Deserialize};
-use std::{fs, io::{self}, process, u8};
+use serde::{Deserialize, Serialize};
+use std::{error::Error, fs, io::{self}, process, u8};
 use serde_json::{from_str, to_string_pretty};
 use clap::Parser;
 use chacha20poly1305::{
     ChaCha20Poly1305, ChaChaPoly1305, Key, Nonce, aead::{Aead, AeadCore, KeyInit, OsRng}
 };
-use argon2::{self, Argon2, password_hash::{rand_core::RngCore}};
+use argon2::{self, Argon2, password_hash::rand_core::{RngCore}};
+
+const SALT_SIZE_BYTES: usize = 16;
+const NONCE_SIZE_BYTES: usize = 12;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Entry {
@@ -23,7 +26,7 @@ struct Cli {
 
 
 #[derive(Parser)]
-enum Commands {
+enum Commands{
     /// Add an entry of service (-s), login (-l) and password (-p) specified.
     Add {
         #[arg(short)]
@@ -41,20 +44,21 @@ enum Commands {
     List,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>>{
     
-    let data = fs::read("data/data.bin").expect("error");
+    let data = fs::read("data/data.bin")?;
     let mut password = String::new();
-    let mut salt = [0u8; 16];
+    let mut salt = [0u8; SALT_SIZE_BYTES];
 
+    // If there is no data yet => 
+    // 1. Init the salt
+    // 2. Ask the user to create a password and to confirm
     if data.is_empty() {
         OsRng.fill_bytes(&mut salt);
         
         println!("Please, choose your password.");
-        
         let mut confirmation = String::new();
-        
-        io::stdin().read_line(&mut password).expect("Not a good pwd");
+        io::stdin().read_line(&mut password)?;
         println!("Confirm your password.");
         io::stdin().read_line(&mut confirmation).expect("Not the same password");
         
@@ -62,24 +66,30 @@ fn main() {
             println!("Password aren't the same!");
             process::abort();
         }
+    // If there is some data already =>
+    // Extract the salt and the password from the data
     } else {
-        let file_data = fs::read("data/data.bin").expect("error"); 
-        let (s, _) = file_data.split_at(16);
+        let file_data = fs::read("data/data.bin")?; 
+        // Keep only the part of data that contains the salt
+        let (s, _) = file_data.split_at(SALT_SIZE_BYTES);
         salt.copy_from_slice(s);
 
         println!("Please, type your password.");
-        io::stdin().read_line(&mut password).expect("Not a good pwd");
+        io::stdin().read_line(&mut password)?;
     }
 
     let key = derive_key(&password, &salt);
     let cipher = ChaChaPoly1305::new(&key);
-
+ 
+    // If there is no data yet, init entries as an empty vec,
+    // otherwise init entries as the old entries decrypted
     let mut entries =
         if data.is_empty() {
             Vec::new()
         } else {
-            let (_, data) = data.split_at(16);
-            decrypt(&data.to_vec(), &cipher)
+            // Keep only the part of data that contains the nonce and entries encrypted
+            let (_, data) = data.split_at(SALT_SIZE_BYTES);
+            decrypt(&data.to_vec(), &cipher)?
         };
 
        
@@ -89,63 +99,70 @@ fn main() {
             // Add the entry that the user wrote to the vector
             entries.push(Entry{ service, login, password});
 
+            // Prepare encrypted, that contains the salt, the nonce and entries encrypted
             let mut encrypted = Vec::new();
             encrypted.extend_from_slice(&salt);
-            encrypted.extend_from_slice(&encrypt(&entries, &cipher)); 
+            encrypted.extend_from_slice(&encrypt(&entries, &cipher)?); 
            
-            fs::write("data/data.bin", encrypted).expect("6");
+            fs::write("data/data.bin", encrypted)?;
+            Ok(())
         }
 
         Commands::Remove { service } => {
             // Keep every entry where the service doesn't correspond to the one that the user want to remove
             entries.retain(|entry| entry.service != service);
 
+            // Prepare encrypted, that contains the salt, the nonce and entries encrypted
             let mut encrypted = Vec::new();
             encrypted.extend_from_slice(&salt);
-            encrypted.extend_from_slice(&encrypt(&entries, &cipher)); 
+            encrypted.extend_from_slice(&encrypt(&entries, &cipher)?); 
 
-            fs::write("data/data.bin", encrypted).expect("6");
+            fs::write("data/data.bin", encrypted)?;
+            Ok(())
         }
 
         Commands::List => {
             // Print the vector
             println!("{:#?}", entries);
+            Ok(())
         }
     }
 }
 
-fn encrypt(entries: &Vec<Entry>, cipher: &ChaCha20Poly1305) -> Vec<u8> {
-    let e_nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); // 96-bits; unique per message
+fn encrypt(entries: &Vec<Entry>, cipher: &ChaCha20Poly1305) -> Result<Vec<u8>, Box<dyn Error>> {
+    // Generate the nonce (12 bytes)
+    let e_nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng); 
 
-    // Rewrote the vector with the value added to the data file
-    let json = to_string_pretty(&entries).expect("4");
-            
+    let json = to_string_pretty(&entries)?;
     let encrypted = cipher.encrypt(&e_nonce, json.as_bytes()).expect("Wrong password!");
 
+    // Return a byte vector that contains the nonce and then the entries encrypted
     let mut out = Vec::new();
     out.extend_from_slice(&e_nonce);
     out.extend_from_slice(&encrypted);
-    out
+    Ok(out)
 }
 
 
-fn decrypt(data: &Vec<u8>, cipher: &ChaCha20Poly1305) -> Vec<Entry> {
-    let (d_nonce_bytes, ciphertext) = data.split_at(12);
+fn decrypt(data: &Vec<u8>, cipher: &ChaCha20Poly1305) -> Result<Vec<Entry>, Box<dyn Error>> {
+    // Extract the nonce (type byte for the moment) and the crypted entries from the data
+    let (d_nonce_bytes, ciphertext) = data.split_at(NONCE_SIZE_BYTES);
     let d_nonce = Nonce::from_slice(d_nonce_bytes);
                 
     let decrypted = cipher.decrypt(&d_nonce, ciphertext).expect("Wrong password!"); 
-    let json = String::from_utf8(decrypted).expect("2"); 
-    from_str::<Vec<Entry>>(&json).expect("3")
+    let json = String::from_utf8(decrypted)?; 
+    Ok(from_str::<Vec<Entry>>(&json)?)
 }
 
 fn derive_key(pwd: &str, salt: &[u8]) -> Key {
     // ChaCha20Poly1305 demands a 256 bits key -> 32 bytes
     let mut key_bytes = [0u8; 32];
 
+    // Create a byte array with the password and the salt
     Argon2::default().hash_password_into(
         pwd.as_bytes(),
         salt,
-        &mut key_bytes).expect("");
+        &mut key_bytes).expect("Argon2 failed");
 
     Key::from_slice(&key_bytes).clone()
 }
